30f6c0180de5dad4219877b66e3af6eea8df666d
tycrek <josh.moore@jmoore.dev>
2019-09-30 12:05:44 -0600

    Added ID downloader

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var fs = require('fs-extra');
 var path = require('path');
+var fetch = require('node-fetch');
 
-const VINE_ID_FILE = 'vine_ids_no_dupes.txt';
+const VINE_ID_URL = 'https://do-space.jmoore.dev/vine_ids_no_dupes.txt';
+const VINE_ID_FILE = path.join(__dirname, 'vine_ids_no_dupes.txt');
 const ARCHIVE_URL = 'https://archive.vine.co/posts/';
 
 
 // First check if the vine_ids.txt file exists
-fs.pathExists(path.join(__dirname, VINE_ID_FILE))
+fs.pathExists(VINE_ID_FILE)
 	.then((exists) => {
-		console.log(exists);
+		exists ? readVineIds() : downloadVineIds();
 	})
 	.catch((err) => console.error(err));
+
+function downloadVineIds() {
+	fetch(VINE_ID_URL)
+		.then((res) => res.text())
+		.then((text) => fs.writeFile(VINE_ID_FILE, text))
+		.then(() => readVineIds());
+}
+
+function readVineIds() {
+	console.log('yeets');
+}
