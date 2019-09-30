bcb45dcfaee3aa5fb0b2b4a6859110e253099984
tycrek <josh.moore@jmoore.dev>
2019-09-30 11:52:45 -0600

    Added checking if file exists

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var fs = require('fs-extra');
+var path = require('path');
+
+const VINE_ID_FILE = 'vine_ids_no_dupes.txt';
 const ARCHIVE_URL = 'https://archive.vine.co/posts/';
+
+
+// First check if the vine_ids.txt file exists
+fs.pathExists(path.join(__dirname, VINE_ID_FILE))
+	.then((exists) => {
+		console.log(exists);
+	})
+	.catch((err) => console.error(err));
